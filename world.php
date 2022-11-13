5736a9b239fec8c682c96c9d52615a8fca5c9057
Zoe Stephens <zoe01stephens@gmail.com>
2022-11-13 13:34:58 -0500

    Added initial starter files

---
+++ world.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$username = 'lab5_user';
+$password = '';
+$dbname = 'world';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$stmt = $conn->query("SELECT * FROM countries");
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<ul>
+<?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>
